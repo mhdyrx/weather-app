8391fdf109ac3469b3789aebdfacc99187f8b9e5
mhdyr <mhdyrghx@gmail.com>
2026-07-03 23:37:21 +0330

    Add wind and humidity features

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const searchBtn = document.querySelector(".search-button");
 const name = document.querySelector(".display-city-name");
 const degree = document.querySelector(".display-degree");
 const weatherImg = document.querySelector(".display-weather-icon");
+const wind = document.querySelector(".display-more-details-desc-wind");
+const humidity = document.querySelector(".display-more-details-desc-humidity");
 
 const getCityCoords = async function (city) {
   const response = await fetch(
@@ -17,43 +19,42 @@ const getCityCoords = async function (city) {
   return data;
 };
 
-const displayWeather = function (data, cityName) {
-  const weatherCode = data.current.weather_code;
-  const isDay = data.current.is_day;
-
-  degree.textContent = Math.round(data.current.temperature_2m) + "°C";
-  name.textContent = cityName;
+const weatherIcon = function (weatherCode, isDay) {
   let weatherImgSource = "node_modules/@meteocons/svg/fill";
 
-  if (weatherCode === 0 && isDay) weatherImgSource += "/clear-day.svg";
-  else if (weatherCode === 0 && !isDay) weatherImgSource += "/clear-night.svg";
-  else if ([1, 2, 3].includes(weatherCode) && isDay)
-    weatherImgSource += "/mostly-clear-day.svg";
-  else if ([1, 2, 3].includes(weatherCode) && !isDay)
-    weatherImgSource += "/mostly-clear-night.svg";
-  else if ([45, 48].includes(weatherCode) && isDay)
-    weatherImgSource += "/partly-cloudy-day-fog.svg";
-  else if ([45, 48].includes(weatherCode) && !isDay)
-    weatherImgSource += "/partly-cloudy-night-fog.svg";
-  else if ([51, 53, 55].includes(weatherCode) && isDay)
-    weatherImgSource += "/partly-cloudy-day-drizzle.svg";
-  else if ([51, 53, 55].includes(weatherCode) && !isDay)
-    weatherImgSource += "/partly-cloudy-night-drizzle.svg";
-  else if ([56, 57].includes(weatherCode) && isDay)
-    weatherImgSource += "/extreme-day-drizzle.svg";
-  else if ([56, 57].includes(weatherCode) && !isDay)
-    weatherImgSource += "/extreme-night-drizzle.svg";
+  if (weatherCode === 0)
+    weatherImgSource += `/clear-${isDay ? "day" : "night"}.svg`;
+  else if ([1, 2, 3].includes(weatherCode))
+    weatherImgSource += `/mostly-clear-${isDay ? "day" : "night"}.svg`;
+  else if ([45, 48].includes(weatherCode))
+    weatherImgSource += `/partly-cloudy-${isDay ? "day" : "night"}-fog.svg`;
+  else if ([51, 53, 55].includes(weatherCode))
+    weatherImgSource += `/partly-cloudy-${isDay ? "day" : "night"}-drizzle.svg`;
+  else if ([56, 57].includes(weatherCode))
+    weatherImgSource += `/extreme-${isDay ? "day" : "night"}-drizzle.svg`;
   else if ([66, 67, 61, 63, 65, 80, 81, 82].includes(weatherCode))
     weatherImgSource += "/rain.svg";
   else if ([71, 73, 75, 77, 85, 86].includes(weatherCode))
     weatherImgSource += "/snow.svg";
   else if ([95, 96, 99].includes(weatherCode))
     weatherImgSource += "/thunderstorms-rain.svg";
+  else weatherImgSource += "/wind.svg";
 
-  weatherImg.src = weatherImgSource;
+  return weatherImgSource;
+};
 
-  console.log(data);
-  console.log(weatherCode);
+const displayWeather = function (data, cityName) {
+  const weatherCode = data.current.weather_code;
+  const isDay = data.current.is_day === 1;
+  const imageSource = weatherIcon(weatherCode, isDay);
+
+  degree.textContent = Math.round(data.current.temperature_2m) + "°C";
+  name.textContent = cityName;
+  weatherImg.src = imageSource;
+
+  // wind.textContent = "";
+  humidity.textContent = data.current.relative_humidity_2m + "%";
+  wind.textContent = data.current.wind_speed_10m + " km/h";
 };
 
 const searchCityData = async function (city) {
@@ -63,23 +64,29 @@ const searchCityData = async function (city) {
   const { lat, lon } = cityData[0];
 
   const response = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=wind_speed_10m,precipitation,temperature_2m,apparent_temperature,relative_humidity_2m,is_day,weather_code`,
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=wind_speed_10m,precipitation,temperature_2m,apparent_temperature,relative_humidity_2m,is_day,weather_code,precipitation`,
   );
 
   const data = await response.json();
 
+  console.log(data);
   displayWeather(data, cityName);
+};
+
+const clearInputEl = function () {
+  searchInput.value = "";
+  searchInput.blur();
 };
 
 searchBtn.addEventListener("click", function () {
   if (!searchInput.value) return;
   searchCityData(searchInput.value);
-  searchInput.value = "";
+  clearInputEl();
 });
 
 searchInput.addEventListener("keydown", function (e) {
   if (!searchInput.value) return;
   if (e.key !== "Enter") return;
   searchCityData(searchInput.value);
-  searchInput.value = "";
+  clearInputEl();
 });
